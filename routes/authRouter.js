b99b985b8083369d1fdcace8f40678c1ec00cc32
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-01-09 19:17:49 +0530

    use bcrypt

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,13 +1,13 @@
 const { Router } = require("express");
 const Users = require("../model/user.js");
-
+const bcrypt= require("bcrypt");
 
 const authRouter = Router();
 
 authRouter.post("/users", async(req, res) => {
   const { name, userName, passWord } = req.body;
-
-  const newuser= await new Users({name:name, userName:userName,passWord: passWord})
+  const hashPassword=  await bcrypt.hash(passWord, 10);
+  const newuser= await new Users({name:name, userName:userName,passWord: hashPassword})
   newuser.save((err, success) => {
     try {
       return res
