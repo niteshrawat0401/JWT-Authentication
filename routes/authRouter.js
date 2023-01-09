78112817f4ee5d37a39ad738487f7caa8f0a4cdb
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-01-09 19:17:04 +0530

    post req

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,18 +1,20 @@
 const { Router } = require("express");
 const Users = require("../model/user.js");
 
+
 const authRouter = Router();
 
 authRouter.post("/users", async(req, res) => {
   const { name, userName, passWord } = req.body;
-  const newuser= await new Users({name, userName, passWord})
+
+  const newuser= await new Users({name:name, userName:userName,passWord: passWord})
   newuser.save((err, success) => {
     try {
       return res
         .status(201)
         .send({ message: "Sign up Successfully", newuser: success["_doc"] });
     } catch (error) {
-      return res.status(500).send({ message: "Something wen wrong" });
+      return res.status(500).send({ message: "Something wen wrong", err });
     }
   });
 });
