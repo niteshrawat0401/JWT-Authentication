3c59e5f379ebeb267c3c89c390ddee6aea6f10d1
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-01-10 19:49:46 +0530

    add

--- controller/jwt.js
+++ controller/jwt.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+// Verify token
 exports.authentication = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.spit(" ")[1];
