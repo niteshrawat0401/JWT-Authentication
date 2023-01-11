faf1b41df2f6addcbe96b334d3c28b45c32a2ba1
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-01-11 14:40:20 +0530

    onchange function

--- client/frontend/src/components/pages/Signup.jsx
+++ client/frontend/src/components/pages/Signup.jsx
@@ -1,20 +1,54 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+let init = {
+  name: "",
+  userName: "",
+  passWord: "",
+};
 
 export const Signup = () => {
+  const [signup, setSignupdata] = useState(init);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setSignupdata({ ...signup, [name]: value });
+  };
+
   return (
     <div>
-        <h2>Register User</h2>
-        <form>
-            <input type="text" name="name" placeholder='Name'/>
-            <br />
-            <input type="text" name="userName" placeholder='UserName'/>
-            <br />
-            <input type="password" name="passWord" placeholder='PassWord' />
-            <br />
-            <input type="submit" value="Sign Up" />
-        </form>
-        <p>Already have an account <Link to={"/login"}>Login</Link></p>
+      <h2>Register User</h2>
+      <form>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <input
+          type="text"
+          name="userName"
+          placeholder="UserName"
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <input
+          type="password"
+          name="passWord"
+          placeholder="PassWord"
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <input type="submit" value="Sign Up" />
+      </form>
+      <p>
+        Already have an account <Link to={"/login"}>Login</Link>
+      </p>
     </div>
-  )
-}
+  );
+};
