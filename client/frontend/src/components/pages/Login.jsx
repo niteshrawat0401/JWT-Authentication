b10207a0bc0255cab0f237db66839a8a83640304
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-01-11 15:07:33 +0530

    handle login function

--- client/frontend/src/components/pages/Login.jsx
+++ client/frontend/src/components/pages/Login.jsx
@@ -1,17 +1,57 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+let init = {
+  userName: "",
+  passWord: "",
+};
 
 export const Login = () => {
+  const [login, setlogindata] = useState(init);
+
+  const handleChange=(e)=>{
+    const {name, value}= e.target;
+    setlogindata({ ...login, [name]: value});
+  }
+    // const handleLogin = (e) => {
+    //   e.preventDefault();
+    //   axios
+    //     .post("http://localhost:8080/auth/signup", signup)
+    //     .then(() => {
+    //       console.log(signup);
+    //       navigate("/login");
+    //     })
+    //     .catch((err) => {
+    //       alert("Something went wrong");
+    //       console.log("error", err);
+    //     });
+    // };
   return (
-    <div><h2>Login User</h2>
-    <form>
-        <input type="text" name="userName" placeholder='UserName'/>
+    <div>
+      <h2>Login User</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          name="userName"
+          placeholder="UserName"
+          onChange={handleChange}
+          required
+        />
         <br />
-        <input type="password" name="passWord" placeholder='PassWord' />
+        <input
+          type="password"
+          name="passWord"
+          placeholder="PassWord"
+          onChange={handleChange}
+          required
+        />
         <br />
         <input type="submit" value="Log In" />
-    </form>
-    <p>Create an account <Link to={"/signup"}>Signup</Link></p>
+      </form>
+      <p>
+        Create an account <Link to={"/signup"}>Signup</Link>
+      </p>
     </div>
-  )
-}
+  );
+};
