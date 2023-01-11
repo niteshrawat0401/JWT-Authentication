2b08dc4eec72b6120b0a82afd8b4e20cda172862
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-01-11 14:30:17 +0530

    set routing

--- client/frontend/src/App.js
+++ client/frontend/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Signup } from './components/pages/Signup';
 import { Routes, Route } from 'react-router-dom';
+import { Login } from './components/pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <Signup/>
       <Routes>
-        <Route />
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/login' element={<Login/>}/>
       </Routes>
     </div>
   );
